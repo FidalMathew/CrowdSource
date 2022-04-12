f9e54818c4afdc84213af66b3055ae5b2a7ea366
Fidal <fidal15perfect@gmail.com>
2022-04-12 12:44:28 +0530

    css added

--- client/src/components/Invest.jsx
+++ client/src/components/Invest.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Navbar from './Navbar';
 function Invest() {
 
     const handleSubmit = (e) => {
@@ -10,15 +10,18 @@ function Invest() {
     return (
         <div>
             <Navbar />
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="from" />
-                <input type="text" name="reciver" />
-                <input type="number" name="amount" />
-                <Link to="https://crust-demo.herokuapp.com/"> <button >Upload image</button> </Link>
-                <input type="text" name="img_link" />
-                <button  >Submit</button>
-            </form>
-        </div>
+            <div className='invest_body'>
+                <form >
+                    <h2>Invest</h2>
+                    <input type="text" name="from" placeholder='from' />
+                    <input type="text" name="reciver" placeholder='reciver' />
+                    <input type="number" name="amount" placeholder='amount' />
+                    <button> <a href="https://crust-demo.herokuapp.com/" target="_blank" rel="noreferrer" style={{ color: 'blue', textDecoration: "none" }}> Upload image </a></button>
+                    <input type="text" name="img_link" placeholder='img_link' />
+                    <button  >Submit</button>
+                </form>
+            </div>
+        </div >
     )
 }
 
